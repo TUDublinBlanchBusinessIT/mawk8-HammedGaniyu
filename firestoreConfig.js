cc9d34bd47e08811be8780cb17663ecb70e76a2b
HammedGaniyu <B00167256@mytudublin.ie>
2025-11-03 16:53:48 +0000

    All Parts Done But 7

--- firestoreConfig.js
+++ firestoreConfig.js
@@ -2,8 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Your Firebase configuration object goes here
-}
+
+  projectId: "ganiyu-firebase-2025",
+  storageBucket: "ganiyu-firebase-2025.firebasestorage.app",
+  apiKey: "AIzaSyD-NYFS92ojX0fmHiRZMIoKkQNTscn0xe8",
+  authDomain: "ganiyu-firebase-2025.firebaseapp.com",
+  messagingSenderId: "467468752799",
+  projectNumber: "467468752799"
+  
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
